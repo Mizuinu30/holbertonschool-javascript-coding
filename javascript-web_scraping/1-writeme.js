fadd91c790dbef586158e3c055cfddee8c2bc322
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-23 19:16:55 -0400

    Write a script that writes a string to a file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,17 +3,18 @@
 const fs = require('fs');
 const process = require('process');
 
-if (process.argv.length <= 2) {
-    console.log("Usage: ./0-readme.js <file_path>");
+if (process.argv.length <= 3) {
+    console.log("Usage: node script.js <file_path> <string_to_write>");
     process.exit(-1);
 }
 
 const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
     if (err) {
         console.error(err);
         return;
     }
-    console.log(data);
+    console.log(`Content written to ${filePath}`);
 });
