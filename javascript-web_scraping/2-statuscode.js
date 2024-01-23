459df11ce46134e3021ac028cfc2c0add7fbb2b2
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-23 19:42:25 -0400

    add a script that displays the status code of a GET request

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
+const process = process.argv[2];
+// Make a GET request to the URL
+if (process.argv.length <= 2) {
+    console.log('Usage: node 2-statuscode.js <URL>');
+    process.exit(-1);
+}
+// Move the declaration of 'url' outside of the block scope
 const url = process.argv[2];
 // Make a GET request to the URL
-request(url, function (error, response, body) {
-  if (error) {
-    console.error('error:', error);
-  } else {
-    console.log('code:', response && response.statusCode);
-  }
+request(url, (error, response, body) => {
+    if (error) {
+        console.log('error:', error);
+        return;
+    }
+    console.log('code:', response.statusCode);
 });
