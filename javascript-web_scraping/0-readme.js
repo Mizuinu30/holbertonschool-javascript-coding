2b65891ff049e119333832cf716b2bcddc1e94cc
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-23 19:24:30 -0400

    fix lint

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
-
+// Read a file
 const fs = require('fs');
-const process = require('process');
-
-// Check if the file path is provided as an argument
-if (process.argv.length <= 2) {
-  console.log("Please provide the file path as an argument.");
-  process.exit(-1);
-}
-// Get the file path
 const filePath = process.argv[2];
 // Read the file
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
