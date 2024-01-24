92465f1fd6cd2d719e3de0e47406ab4c3a6dd309
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-23 20:15:10 -0400

    2-statuscode.js

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = process.argv[2];
-
-request(url, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-        return;
-    }
-    console.log(`code: ${response && response.statusCode}`);
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
