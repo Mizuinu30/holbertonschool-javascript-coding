78109e71efa5aa4577f378dac9a97babe406b55c
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-23 20:06:06 -0400

    Add a script that gets a title

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 // Print the title of a Star Wars movie where the episode number matches a given integer.
 const request = require('request');
 const MovieId = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + MovieId;
+const url = 'https://swapi-api.hbtn.io/api/films/id';
 // Make a request to the URL
 request.get(url, function (err, response, body) {
   if (err) {
