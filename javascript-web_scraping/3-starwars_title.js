85728daeaaddc8f4797b3a29f089b96f777caf53
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-23 20:19:25 -0400

    3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,18 +1,16 @@
 #!/usr/bin/node
-/*
-     Prints the title of a Star Wars movie
-     where the episode number matches a given integer
-*/
-const request = require('request');
-const movieID = parseInt(process.argv[2]);
-const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
+const request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+// Make a GET request to the first argument and print the title
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
   } else {
-    if (response.statusCode === 200) {
-      console.log(JSON.parse(body).title);
-    }
+    console.log('Error code: ' + response.statusCode);
   }
 });
