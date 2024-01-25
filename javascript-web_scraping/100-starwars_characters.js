02b6bb58209fec60661d8202e99b3193af96b319
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-25 19:22:55 -0400

    100-starwars_characters.js

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
 
-const request = require('request');
+const req = require('request');
 const id = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${id}`;
-request.get(url + id, function (err, response, body) {
-    if (err) {
-        console.log(err);
-    }
-    const data = JSON.parse(body);
-    const dd = data.characters;
-    for (const i in dd) {
-        request.get(dd[i], function (err, response, body1) {
-            if (err) {
-                console.log(err);
-            }
-            const data1 = JSON.parse(body1);
-            console.log(data1.name);
-        });
-    }
+const url = 'https://swapi-api.hbtn.io/api/films/';
+req.get(url + id, function (error, res, body) {
+  if (error) {
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+  const dd = data.characters;
+  for (const i of dd) {
+    req.get(i, function (error, res, body1) {
+      if (error) {
+        console.log(error);
+      }
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
+    });
+  }
 });
