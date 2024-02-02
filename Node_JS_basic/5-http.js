c62084cfe39527a81dcfbd72a5c98809f4b5cb2d
Mizuinu30 <juansilva.dvm@gmail.com>
2024-02-02 18:18:31 -0400

    Add a small HTTP server using the http module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,26 +1,30 @@
 /* eslint-disable */
 const http = require('http');
-const fs = require('fs').promises;
+const countStudents = require('./3-read_file_async');
+
+const hostname = '127.0.0.1';
+const port = 1245;
 
 const app = http.createServer(async (req, res) => {
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello Holberton School!');
-    } else if (req.url === '/students') {
-        try {
-            const data = await fs.readFile('3-read_file_async.js', 'utf8');
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end(`This is the list of our students\n${data}`);
-        } catch (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Error reading file');
-        }
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Not Found');
-    }
+  res.statusCode = 200;
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    let dbInfo = 'This is the list of our students\n';
+    await countStudents(process.argv[2])
+      .then((msg) => {
+        dbInfo += msg;
+        res.end(dbInfo);
+      })
+      .catch((err) => {
+        dbInfo += err.message;
+        res.end(dbInfo);
+      });
+  }
 });
 
-app.listen(1245);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
 
 module.exports = app;
