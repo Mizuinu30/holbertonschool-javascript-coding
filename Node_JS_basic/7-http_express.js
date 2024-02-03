977f1c866f7bc13f4e27ee3534a96a3a5a334e30
Mizuinu30 <juansilva.dvm@gmail.com>
2024-02-02 20:19:05 -0400

    Add 7-http_express.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,21 +1,29 @@
 const express = require('express');
-const fs = require('fs').promises;
+
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = args[0];
 
 const app = express();
+const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
+  const msg = 'This is the list of our students\n';
   try {
-    const data = await fs.readFile('3-read_file_async.js', 'utf8');
-    res.send(`This is the list of our students\n${data}`);
-  } catch (err) {
-    res.status(500).send('Error reading file');
+    const students = await countStudents(DATABASE);
+    res.send(`${msg}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${msg}${error.message}`);
   }
 });
 
-app.listen(1245);
+app.listen(port, () => {
+  //   console.log(`Example app listening at http://localhost:${port}`);
+});
 
 module.exports = app;
